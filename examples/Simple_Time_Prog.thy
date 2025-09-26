57569d9898014919530f8cc8002b31c98f88599e
Simon Foster <simondf@gmail.com>
2025-09-26 16:43:04 +0100

    Added commentary on simple timed relation example

--- examples/Simple_Time_Prog.thy
+++ examples/Simple_Time_Prog.thy
@@ -1,11 +1,17 @@
+section \<open> Simple Time Programs \<close>
+
 theory Simple_Time_Prog
   imports Simple_Time_Theory
 begin
+
+text \<open> A timed program type is created, corresponding to the healthy timed relations. \<close>
 
 typedef 's tprog = "{P :: 's time_rel. P is HT}"
   using Delay_HT by blast
 
 setup_lifting type_definition_tprog
+
+text \<open> We can also show the other operators can be lifted to operate over the new type. \<close>
 
 lift_definition Delay :: "nat \<Rightarrow> 's tprog" is "Simple_Time_Theory.Delay" by (simp add: Delay_HT)
 
--- examples/Simple_Time_Theory.thy
+++ examples/Simple_Time_Theory.thy
@@ -1,31 +1,50 @@
+section \<open> A Simple Time UTP Theory \<close>
+
 theory Simple_Time_Theory
   imports "UTP2.utp"
 begin
+
+text \<open> We first create our alphabet, which has components for the time and state. \<close>
 
 alphabet 's time_alpha =
   time  :: "nat"
   state :: "'s"
 
+text \<open> A timed relation is a homogeneous relation over this alphabet type. \<close>
+
 type_synonym 's time_rel = "'s time_alpha hrel"
 
+text \<open> We define an operator for introducing a delay of $n$ time unit, by incrementing time and
+  leaving the state unchanged. \<close>
+
 definition Delay :: "nat \<Rightarrow> 's time_alpha hrel" where
-[pred]: "Delay(n) = (time\<^sup>> = time\<^sup>< + \<guillemotleft>n\<guillemotright>)\<^sub>e"
+[pred]: "Delay(n) = (time\<^sup>> = time\<^sup>< + \<guillemotleft>n\<guillemotright> \<and> state\<^sup>> = state\<^sup><)\<^sub>e"
+
+text \<open> We also define an assignment operator that applies an assignment substitution function
+  to the state, and leaves time unchanged. \<close>
+
+definition assigns_trel :: "'s subst \<Rightarrow> 's time_rel" where
+[pred]: "assigns_trel \<sigma> = (state\<^sup>> = \<guillemotleft>\<sigma>\<guillemotright>(state\<^sup><) \<and> time\<^sup>> = time\<^sup><)\<^sub>e"
+
+text \<open> We also define a healthiness condition that ensure that time monotonically increases. \<close>
 
 definition HT :: "'s time_alpha hrel \<Rightarrow> 's time_alpha hrel" where
 [pred]: "HT(P) = (P \<and> (time\<^sup>< \<le> time\<^sup>>)\<^sub>e)"
 
+text \<open> We can then prove that @{const Delay} is a healthy operator, since it increments time. This
+  is done as an Isar proof, but it could be fully automated by Isabelle. \<close>
+
 lemma Delay_HT [closure]: "Delay(n) is HT"
 proof (rule Healthy_intro)
-  have "HT (Delay n) = ((time\<^sup>> = time\<^sup>< + \<guillemotleft>n\<guillemotright>)\<^sub>e \<and> (time\<^sup>< \<le> time\<^sup>>)\<^sub>e)"
+  have "HT (Delay n) = ((time\<^sup>> = time\<^sup>< + \<guillemotleft>n\<guillemotright> \<and> state\<^sup>> = state\<^sup><)\<^sub>e \<and> (time\<^sup>< \<le> time\<^sup>>)\<^sub>e)"
     by (simp add: HT_def Delay_def)
-  also have "... = (time\<^sup>> = time\<^sup>< + \<guillemotleft>n\<guillemotright>)\<^sub>e"
+  also have "... = (time\<^sup>> = time\<^sup>< + \<guillemotleft>n\<guillemotright> \<and> state\<^sup>> = state\<^sup><)\<^sub>e"
     by pred_auto
   finally show "HT (Delay n) = Delay n"
     by (simp add: Delay_def)
 qed
 
-definition assigns_trel :: "'s subst \<Rightarrow> 's time_rel" where
-[pred]: "assigns_trel \<sigma> = (state\<^sup>> = \<guillemotleft>\<sigma>\<guillemotright>(state\<^sup><) \<and> time\<^sup>> = time\<^sup><)\<^sub>e"
+text \<open> Similarly, assignment is healthy, using the proof method @{method pred_auto}. \<close>
 
 lemma assigns_HT: "assigns_trel \<sigma> is HT"
   by (pred_auto)
@@ -58,6 +77,8 @@ proof (rule MonotonicI)
   thus "HT(P) \<sqsubseteq> HT(Q)"
     by pred_simp
 qed
+
+text \<open> We can prove that @{const HT} is closed under sequential composition. \<close>
 
 lemma HT_seq:
   assumes "P is HT" "Q is HT"
